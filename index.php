705106f026b4e79267e090689e01842048909938
Jerome <jerome.neupert@epitech.eu>
2023-04-11 15:02:05 +0200

    Ajout de la class Core

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
-<?php include("src/View/User/login.php"); ?>
 <pre>
-<?php
-print_r($_POST);
-print_r($_GET);
-print_r($_SERVER);
-?>
+    <?php
+    include_once("./src/View/User/login.php");
+    print_r($_GET);
+    print_r($_POST);
+    print_r($_SERVER); 
+    ?>
 </pre>
+
+<?php
+define('BASE_URI',str_replace('\\','/',substr(__DIR__,strlen($_SERVER['DOCUMENT_ROOT']))));
+require_once("./Core/Core.php");
+
+$app= new Core\Core();
+$app->run();
